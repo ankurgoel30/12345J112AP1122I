de50752d286e8791e72ca978dc2cc33153cb1ec6
surabhi <surabhi.bhawsar@pepcus.com>
2017-11-06 21:10:52 +0530

    THR-3704: Couple of improvements are done to handle error conditions and limiting the records to get from the getCompanies. Modified application.properties to connect with thinkhr development database.

--- src/test/java/com/thinkhr/external/api/repositories/CompanyRepositoryTest.java
+++ src/test/java/com/thinkhr/external/api/repositories/CompanyRepositoryTest.java
@@ -1,10 +1,5 @@
 package com.thinkhr.external.api.repositories;
-import static com.thinkhr.external.api.utils.ApiTestDataUtil.createCompany;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
-
-import java.util.Date;
-import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,8 +8,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.thinkhr.external.api.db.entities.Company;
 
 /**
  * Junit to verify methods of CompanyRepository with use of H2 database
@@ -38,19 +31,7 @@ public class CompanyRepositoryTest {
 	 */
 	@Test
 	public void testFindAll() {
-		//given
-		Company company1 = createCompany(null, "Pepcus", "Software", "PEP");
-		company1.setSearchHelp("Test");
-		company1.setCompanySince(new Date());
-		company1.setSpecialNote("111");
-		entityManager.persist(company1);
-		entityManager.flush();
-		
-		//when
-		List<Company> companies = (List)companyRepository.findAll();
-
-		//then
-		assertThat(companies.size()).isGreaterThan(0);
+		   //TODO: Add implementation	
 	}
 
 	/**
@@ -58,26 +39,20 @@ public class CompanyRepositoryTest {
 	 */
 	@Test
 	public void testFindOne() {
-		//given
-		Company company1 = createCompany(null, "Pepcus2", "Software", "PEP2");
-		company1.setSearchHelp("Test3");
-		company1.setCompanySince(new Date());
-		company1.setSpecialNote("TEST3");
-		entityManager.persist(company1);
-		entityManager.flush();
-
-		//when
-		Company compFromRepo = companyRepository.findOne(company1.getCompanyId());
-
-		//then
-		assertThat(compFromRepo.getCompanyName()).isEqualTo(compFromRepo.getCompanyName());
+		   //TODO: Add implementation	
 	}
 	
+	/**
+	 * To test save method
+	 */
 	@Test
 	public void testSave() {
 	   //TODO: Add implementation	
 	}
 	
+	/**
+	 * To test delete method
+	 */
 	@Test
 	public void testDelete() {
 		//TODO: Add implementation
