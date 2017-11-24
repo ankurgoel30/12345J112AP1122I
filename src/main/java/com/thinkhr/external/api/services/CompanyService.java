197bf72ad95bf59161b5397e55a66feb91ab12a7
asi-ajain <ajay.jain@pepcus.com>
2017-11-24 23:58:56 +0530

    THR-3839: Some unused imports are removed.

--- src/main/java/com/thinkhr/external/api/services/CompanyService.java
+++ src/main/java/com/thinkhr/external/api/services/CompanyService.java
@@ -12,13 +12,10 @@ import static com.thinkhr.external.api.services.utils.EntitySearchUtil.getPageab
 import java.io.IOException;
 import java.sql.DataTruncation;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
