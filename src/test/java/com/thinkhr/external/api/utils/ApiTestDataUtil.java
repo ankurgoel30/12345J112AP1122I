d27dda6273f8615a8442c461bf005c8d696b86e3
jainajay <ajay.jain@pepcus.com>
2017-11-30 19:01:06 +0530

    THR-3838: Added Junits for FileImportValidatorTest and CommonServiceTest.java

--- src/test/java/com/thinkhr/external/api/utils/ApiTestDataUtil.java
+++ src/test/java/com/thinkhr/external/api/utils/ApiTestDataUtil.java
@@ -13,7 +13,9 @@ import org.springframework.http.ResponseEntity;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thinkhr.external.api.db.entities.Company;
+import com.thinkhr.external.api.db.entities.CustomFields;
 import com.thinkhr.external.api.db.entities.User;
+import com.thinkhr.external.api.model.FileImportResult;
 
 /**
  * Utility class to keep all utilities required for Junits
@@ -24,183 +26,250 @@ import com.thinkhr.external.api.db.entities.User;
  */
 public class ApiTestDataUtil {
 
-    public static final String API_BASE_PATH = "/v1/";
-    public static final String COMPANY_API_BASE_PATH = "/v1/companies/";
-    public static final String USER_API_BASE_PATH = "/v1/users/";
-    public static final String COMPANY_API_REQUEST_PARAM_OFFSET = "offset";
-    public static final String COMPANY_API_REQUEST_PARAM_LIMIT = "limit";
-    public static final String COMPANY_API_REQUEST_PARAM_SORT = "sort";
-    public static final String COMPANY_API_REQUEST_PARAM_SEARCH_SPEC = "searchSpec";
+	public static final String API_BASE_PATH = "/v1/";
+	public static final String COMPANY_API_BASE_PATH = "/v1/companies/";
+	public static final String USER_API_BASE_PATH = "/v1/users/";
+	public static final String COMPANY_API_REQUEST_PARAM_OFFSET = "offset";
+	public static final String COMPANY_API_REQUEST_PARAM_LIMIT = "limit";
+	public static final String COMPANY_API_REQUEST_PARAM_SORT = "sort";
+	public static final String COMPANY_API_REQUEST_PARAM_SEARCH_SPEC = "searchSpec";
+	
+	/**
+	 * Convert object into Json String
+	 * 
+	 * @param object
+	 * @param kclass
+	 * @return
+	 * @throws JAXBException 
+	 * @throws PropertyException 
+	 */
+	public static String getJsonString(Object object) throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setSerializationInclusion(Include.NON_NULL);
+		return mapper.writeValueAsString(object);	
+	}
+	
+
+	/**
+	 * Create a Company Model for given inputs
+	 * 
+	 * @param companyId
+	 * @param companyName
+	 * @param companyType
+	 * @param displayName
+	 * @param companySince
+	 * @param specialNotes
+	 * @return
+	 */
+	public static Company createCompany(Integer companyId, String companyName,
+			String companyType, String displayName, 
+			Date companySince, String specialNotes, String searchHelp) {
+		Company company = new Company();
+		if (companyId != null) {
+			company.setCompanyId(companyId);
+		}
+		company.setCompanyName(companyName);
+		company.setCompanyType(companyType);
+		company.setDisplayName(displayName);
+		company.setCompanySince(companySince);
+		company.setSpecialNote(specialNotes);
+		company.setSearchHelp(searchHelp); 
+		company.setIsActive(1);
+		return company;
+	}
+	
+	/**
+	 * Create a User entity for given inputs
+	 * 
+	 * @param contactId
+	 * @param firstName
+	 * @param lastName
+	 * @param searchHelp
+	 * @return
+	 */
+	public static User createUser(Integer userId, String firstName, String lastName,
+			String email, String userName, String companyName) {
+		User user = new User();
+		if (userId != null) {
+			user.setUserId(userId);
+		}
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setEmail(email);
+		user.setUserName(userName);
+		user.setCompanyName(companyName);   
+		return user;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static User createUser() {
+		return createUser(1, "Surabhi", "Bhawsar", "surabhi.bhawsar@pepcus.com", "sbhawsar", "Pepcus");
+	}
+	
+	/**
+	 * Creates a user response object
+	 * 
+	 * @param user
+	 * @param httpStatus
+	 * @return
+	 */
+	public static ResponseEntity<User> createUserResponseEntity(User user, HttpStatus httpStatus) {
+		return new ResponseEntity<User>(user, httpStatus);
+	}
+
+	/**
+	 * Creates a company response object
+	 * 
+	 * @param company
+	 * @param httpStatus
+	 * @return
+	 */
+	public static ResponseEntity<Company> createCompanyResponseEntity(Company company, HttpStatus httpStatus) {
+		return new ResponseEntity<Company>(company, httpStatus);
+	}
+	
+	/**
+	 * createCompanyIdResponseEntity
+	 * 
+	 * @param companyId
+	 * @param httpStatus
+	 * @return
+	 */
+	public static ResponseEntity<Integer> createCompanyIdResponseEntity(Integer companyId, HttpStatus httpStatus) {
+		return new ResponseEntity<Integer>(companyId, httpStatus);
+	}
+	
+	/**
+	 * createContactIdResponseEntity
+	 * 
+	 * @param contactId
+	 * @param httpStatus
+	 * @return
+	 */
+	public static ResponseEntity<Integer> createUserIdResponseEntity(Integer userId, HttpStatus httpStatus) {
+		return new ResponseEntity<Integer>(userId, httpStatus);
+	}
+
+	/**
+	 * Create Model object for Company
+	 * 
+	 * @return
+	 */
+	public static Company createCompany() {
+		return createCompany(1, "Pepcus", "Software", "PEP", new Date(), "Special", "This is search help");
+	}
+
+	/**
+	 * Create List for Company objects
+	 * 
+	 * @return
+	 */
+	public static List<Company> createCompanies() {
+		List<Company> companies = new ArrayList<Company>();
+		companies.add(createCompany(1, "Pepcus", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help1"));
+		companies.add(createCompany(2, "Google", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help2"));
+		companies.add(createCompany(3, "Facebook", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help3"));
+		companies.add(createCompany(4, "Suzuki", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help4"));
+		companies.add(createCompany(5, "General Motors", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help5"));
+		companies.add(createCompany(6, "L & T", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help6"));
+		companies.add(createCompany(7, "General Electric", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help7"));
+		companies.add(createCompany(8, "Oracle", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help8"));
+		companies.add(createCompany(9, "Microsoft", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help9"));
+		companies.add(createCompany(10, "Thinkhr", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help10"));
+		return companies;
+
+	}
+	
+	/**
+	 * Create List for User objects
+	 * 
+	 * @return
+	 */
+	public static List<User> createUserList() {
+		List<User> users = new ArrayList<User>();
+		
+		users.add(createUser(1, "Isha", "Khandelwal", "isha.khandelwal@gmail.com", "ishaa", "ThinkHR"));
+		users.add(createUser(2, "Sharmila", "Tagore", "stagore@gmail.com", "stagore", "ASI"));
+		users.add(createUser(3, "Surabhi", "Bhawsar", "sbhawsar@gmail.com", "sbhawsar", "Pepcus"));
+		users.add(createUser(4, "Shubham", "Solanki", "ssolanki@gmail.com", "ssolanki", "Pepcus"));
+		users.add(createUser(5, "Ajay", "Jain", "ajain@gmail.com", "ajain", "TCS"));
+		users.add(createUser(6, "Sandeep", "Vishwakarma", "svishwakarma@gmail.com", "svishwakarma", "CIS"));
+		users.add(createUser(7, "Sushil", "Mahajan", "smahajan@gmail.com", "smahajan", "ASI"));
+		users.add(createUser(8, "Sumedha", "Wani", "swani@gmail.com", "swani", "InfoBeans"));
+		users.add(createUser(9, "Mohit", "Jain", "mjain@gmail.com", "mjain", "Pepcus"));
+		users.add(createUser(10, "Avi", "Jain", "ajain@gmail.com", "ajain", "Pepcus"));
+		
+		return users;
+	}
 
     /**
-     * Convert object into Json String
+     * Create List for CustomFields object
      * 
-     * @param object
-     * @param kclass
      * @return
-     * @throws JAXBException 
-     * @throws PropertyException 
      */
-    public static String getJsonString(Object object) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setSerializationInclusion(Include.NON_NULL);
-        return mapper.writeValueAsString(object);	
+    public static List<CustomFields> createCustomFieldsList() {
+        List<CustomFields> customFields = new ArrayList<CustomFields>();
+
+        customFields.add(createCustomFields(2, 15472, "COMPANY", "1", "c1", "CORRELATION_ID", "0"));
+        customFields.add(createCustomFields(3, 15472, "COMPANY", "2", "c2", "GROUP_ID", "0"));
+        return customFields;
     }
 
-
-    /**
-     * Create a Company Model for given inputs
-     * 
-     * @param companyId
-     * @param companyName
-     * @param companyType
-     * @param displayName
-     * @param companySince
-     * @param specialNotes
-     * @return
-     */
-    public static Company createCompany(Integer companyId, String companyName,
-            String companyType, String displayName, 
-            Date companySince, String specialNotes, String searchHelp) {
-        Company company = new Company();
-        if (companyId != null) {
-            company.setCompanyId(companyId);
+    public static CustomFields createCustomFields(Integer id, Integer companyId, String customFieldType,
+            String customFieldColumnName, String customFieldDisplayKey, String customFieldDisplayLabel,
+            String isImportRequired) {
+        CustomFields customFields = new CustomFields();
+        if (id != null) {
+            customFields.setId(id);
         }
-        company.setCompanyName(companyName);
-        company.setCompanyType(companyType);
-        company.setDisplayName(displayName);
-        company.setCompanySince(companySince);
-        company.setSpecialNote(specialNotes);
-        company.setSearchHelp(searchHelp); 
-        company.setIsActive(1);
-        return company;
+        customFields.setCompanyId(companyId);
+        customFields.setCustomFieldType(customFieldType);
+        customFields.setCustomFieldColumnName(customFieldColumnName);
+        customFields.setCustomFieldDisplayKey(customFieldDisplayKey);
+        customFields.setCustomFieldDisplayLabel(customFieldDisplayLabel);
+        customFields.setIsImportRequired(isImportRequired);
+
+        return customFields;
     }
 
-    /**
-     * Create a User entity for given inputs
-     * 
-     * @param contactId
-     * @param firstName
-     * @param lastName
-     * @param searchHelp
-     * @return
-     */
-    public static User createUser(Integer userId, String firstName, String lastName,
-            String email, String userName, String companyName) {
-        User user = new User();
-        if (userId != null) {
-            user.setUserId(userId);
-        }
-        user.setFirstName(firstName);
-        user.setLastName(lastName);
-        user.setEmail(email);
-        user.setUserName(userName);
-        user.setCompanyName(companyName);   
-        return user;
+    public static String getCsvRecord() {
+        String record = "Adams Radio of Tallahassee LLC 123,Adams Radio of Tallahassee LLC,9522320916,3000 Olson Rd,,Tallahassee,FL,32308,Other,0,,5331058,56,11078286,Non PEO";
+        return record;
     }
 
-    /**
-     * 
-     * @return
-     */
-    public static User createUser() {
-        return createUser(1, "Surabhi", "Bhawsar", "surabhi.bhawsar@pepcus.com", "sbhawsar", "Pepcus");
+    public static String getCsvRecordForDuplicate() {
+        String record = "Pepcus,Adams Radio of Tallahassee LLC,9522320916,3000 Olson Rd,,Tallahassee,FL,32308,Other,0,,5331058,56,11078286,Non PEO";
+        return record;
     }
 
-    /**
-     * Creates a user response object
-     * 
-     * @param user
-     * @param httpStatus
-     * @return
-     */
-    public static ResponseEntity<User> createUserResponseEntity(User user, HttpStatus httpStatus) {
-        return new ResponseEntity<User>(user, httpStatus);
+    public static String getCsvRecordForSpecialCase() {
+        String record = "Paychex,Adams Radio of Tallahassee LLC,9522320916,3000 Olson Rd,,Tallahassee,FL,32308,Other,0,,5331058,56,11078286,Non PEO";
+        return record;
     }
 
-    /**
-     * Creates a company response object
-     * 
-     * @param company
-     * @param httpStatus
-     * @return
-     */
-    public static ResponseEntity<Company> createCompanyResponseEntity(Company company, HttpStatus httpStatus) {
-        return new ResponseEntity<Company>(company, httpStatus);
+    public static FileImportResult createFileImportResultWithNoFailedRecords() {
+        FileImportResult fileImportResult = new FileImportResult();
+        fileImportResult.setTotalRecords(10);
+        fileImportResult.setNumSuccessRecords(10);
+        fileImportResult.setNumFailedRecords(0);
+        return fileImportResult;
     }
 
-    /**
-     * createCompanyIdResponseEntity
-     * 
-     * @param companyId
-     * @param httpStatus
-     * @return
-     */
-    public static ResponseEntity<Integer> createCompanyIdResponseEntity(Integer companyId, HttpStatus httpStatus) {
-        return new ResponseEntity<Integer>(companyId, httpStatus);
-    }
-
-    /**
-     * createContactIdResponseEntity
-     * 
-     * @param contactId
-     * @param httpStatus
-     * @return
-     */
-    public static ResponseEntity<Integer> createUserIdResponseEntity(Integer userId, HttpStatus httpStatus) {
-        return new ResponseEntity<Integer>(userId, httpStatus);
-    }
-
-    /**
-     * Create Model object for Company
-     * 
-     * @return
-     */
-    public static Company createCompany() {
-        return createCompany(1, "Pepcus", "Software", "PEP", new Date(), "Special", "This is search help");
-    }
-
-    /**
-     * Create List for Company objects
-     * 
-     * @return
-     */
-    public static List<Company> createCompanies() {
-        List<Company> companies = new ArrayList<Company>();
-        companies.add(createCompany(1, "Pepcus", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help1"));
-        companies.add(createCompany(2, "Google", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help2"));
-        companies.add(createCompany(3, "Facebook", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help3"));
-        companies.add(createCompany(4, "Suzuki", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help4"));
-        companies.add(createCompany(5, "General Motors", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help5"));
-        companies.add(createCompany(6, "L & T", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help6"));
-        companies.add(createCompany(7, "General Electric", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help7"));
-        companies.add(createCompany(8, "Oracle", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help8"));
-        companies.add(createCompany(9, "Microsoft", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help9"));
-        companies.add(createCompany(10, "Thinkhr", "IT", "PEP", new Date(), "IT comp at Indore", "This is search help10"));
-        return companies;
-
-    }
-
-    /**
-     * Create List for User objects
-     * 
-     * @return
-     */
-    public static List<User> createUserList() {
-        List<User> users = new ArrayList<User>();
-
-        users.add(createUser(1, "Isha", "Khandelwal", "isha.khandelwal@gmail.com", "ishaa", "ThinkHR"));
-        users.add(createUser(2, "Sharmila", "Tagore", "stagore@gmail.com", "stagore", "ASI"));
-        users.add(createUser(3, "Surabhi", "Bhawsar", "sbhawsar@gmail.com", "sbhawsar", "Pepcus"));
-        users.add(createUser(4, "Shubham", "Solanki", "ssolanki@gmail.com", "ssolanki", "Pepcus"));
-        users.add(createUser(5, "Ajay", "Jain", "ajain@gmail.com", "ajain", "TCS"));
-        users.add(createUser(6, "Sandeep", "Vishwakarma", "svishwakarma@gmail.com", "svishwakarma", "CIS"));
-        users.add(createUser(7, "Sushil", "Mahajan", "smahajan@gmail.com", "smahajan", "ASI"));
-        users.add(createUser(8, "Sumedha", "Wani", "swani@gmail.com", "swani", "InfoBeans"));
-        users.add(createUser(9, "Mohit", "Jain", "mjain@gmail.com", "mjain", "Pepcus"));
-        users.add(createUser(10, "Avi", "Jain", "ajain@gmail.com", "ajain", "Pepcus"));
-
-        return users;
+    public static FileImportResult createFileImportResultWithFailedRecords() {
+        FileImportResult fileImportResult = new FileImportResult();
+        fileImportResult.setTotalRecords(10);
+        fileImportResult.setNumSuccessRecords(7);
+        fileImportResult.setNumSuccessRecords(3);
+        fileImportResult.addFailedRecord(3, "", "Blank Record", "Skipped");
+        fileImportResult.addFailedRecord(6,
+                "Paychex,Adams Radio of Tallahassee LLC,9522320916,3000 Olson Rd,,Tallahassee,FL,32308,Other,0,,5331058,56,11078286,Non PEO",
+                "Duplicate", "Skipped");
+        fileImportResult.addFailedRecord(6, "Paychex,Adams Radio of Tallahassee LLC,", "Missing Fields in Record",
+                "Record not added");
+        return fileImportResult;
     }
 
 }
