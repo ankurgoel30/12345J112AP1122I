92aa8e6169509d73f0adee41c478b35130bbde77
surabhi <surabhi.bhawsar@pepcus.com>
2017-12-08 15:41:07 +0530

    THR-3841: Added missing import.

--- src/main/java/com/thinkhr/external/api/services/UserService.java
+++ src/main/java/com/thinkhr/external/api/services/UserService.java
@@ -7,6 +7,7 @@ import static com.thinkhr.external.api.ApplicationConstants.CONTACT;
 import static com.thinkhr.external.api.ApplicationConstants.USER_COLUMN_CLIENT_ID;
 import static com.thinkhr.external.api.ApplicationConstants.USER_COLUMN_PASSWORD;
 import static com.thinkhr.external.api.ApplicationConstants.DEFAULT_PASSWORD;
+import static com.thinkhr.external.api.ApplicationConstants.USER;
 import static com.thinkhr.external.api.request.APIRequestHelper.setRequestAttribute;
 import static com.thinkhr.external.api.response.APIMessageUtil.getMessageFromResourceBundle;
 import static com.thinkhr.external.api.services.upload.FileImportValidator.validateAndGetFileContent;
