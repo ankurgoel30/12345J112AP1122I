5bc9675b3cac97c6d073461e245950ac037e309c
sbhawsar <surabhi.bhawsar@pepcus.com>
2018-01-05 21:42:06 +0530

    THR-4108: Added missing import in User.java.

--- src/main/java/com/thinkhr/external/api/db/entities/User.java
+++ src/main/java/com/thinkhr/external/api/db/entities/User.java
@@ -16,6 +16,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
