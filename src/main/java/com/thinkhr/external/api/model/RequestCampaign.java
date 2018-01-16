ad1dc3dd9e910f8b2421c3d396af91d35747da3f
jnajayjain <ajay.jain@pepcus.com>
2018-01-16 19:10:41 +0530

    THR-4178 : Added Initial Implementation for sending mail through Marketo.

---
+++ src/main/java/com/thinkhr/external/api/model/RequestCampaign.java
@@ -0,0 +1,107 @@
+package com.thinkhr.external.api.model;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+import com.springml.marketo.rest.client.LeadDatabaseClient;
+
+public class RequestCampaign {
+    private String endpoint;
+    public List<Integer> leads = new ArrayList<Integer>();
+    public List<JsonObject> tokens = new ArrayList<JsonObject>();
+
+    public RequestCampaign(String baseUri, int campaignId) {
+        this.endpoint = baseUri + "/rest/v1/campaigns/" + campaignId + "/trigger.json";
+    }
+
+    public RequestCampaign setLeads(ArrayList<Integer> leads) {
+        this.leads = leads;
+        return this;
+    }
+
+    public RequestCampaign addLead(int lead) {
+        leads.add(lead);
+        return this;
+    }
+
+    public RequestCampaign setTokens(ArrayList<JsonObject> tokens) {
+        this.tokens = tokens;
+        return this;
+    }
+
+    public RequestCampaign addToken(String tokenKey, String val) {
+        JsonObject jo = new JsonObject().add("name", tokenKey);
+        jo.add("value", val);
+        tokens.add(jo);
+        return this;
+    }
+
+    public JsonObject postData(String accessToken) {
+        JsonObject result = null;
+        try {
+            JsonObject requestBody = buildRequest(); //builds the Json Request Body
+
+            String s = endpoint + "?access_token=" + accessToken; //takes the endpoint URL and appends the access_token parameter to authenticate
+
+            //System.out.println("Executing RequestCampaign calln" + "Endpoint: " + s + "nRequest Body:n" + requestBody);
+            URL url = new URL(s);
+            HttpsURLConnection urlConn = (HttpsURLConnection) url.openConnection(); //Return a URL connection and cast to HttpsURLConnection
+            urlConn.setRequestMethod("POST");
+            urlConn.setRequestProperty("Content-type", "application/json");
+            urlConn.setRequestProperty("accept", "text/json");
+            urlConn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(urlConn.getOutputStream());
+            wr.write(requestBody.toString());
+            wr.flush();
+
+            InputStream inStream = urlConn.getInputStream(); //get the inputStream from the URL connection
+            Reader reader = new InputStreamReader(inStream);
+            result = JsonObject.readFrom(reader); //Read from the stream into a JsonObject
+
+            System.out.println("Result:n" + result);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    /**
+     * Takes the input and assembles a json representation to submit
+     * @return
+     */
+    private JsonObject buildRequest() {
+        JsonObject requestBody = new JsonObject(); //Create a new JsonObject for the Request Body
+        JsonObject input = new JsonObject();
+        JsonArray leadsArray = new JsonArray();
+        for (int lead : leads) {
+            JsonObject jo = new JsonObject().add("id", lead);
+            leadsArray.add(jo);
+        }
+        input.add("leads", leadsArray);
+
+        //assemble array of tokens and add to input if present
+        if (tokens != null) {
+            JsonArray tokensArray = new JsonArray();
+            for (JsonObject jo : tokens) {
+                tokensArray.add(jo);
+            }
+            input.add("tokens", tokensArray);
+        }
+        requestBody.add("input", input);
+        return requestBody;
+    }
+}
--- src/main/java/com/thinkhr/external/api/services/email/MarketoEmailService.java
+++ src/main/java/com/thinkhr/external/api/services/email/MarketoEmailService.java
@@ -1,8 +1,38 @@
 package com.thinkhr.external.api.services.email;
 
+import static com.thinkhr.external.api.ApplicationConstants.RESET_PASSWORD_PREFIX;
+import static com.thinkhr.external.api.services.utils.CommonUtil.generateHashedValue;
+import static com.thinkhr.external.api.services.utils.EmailUtil.BROKER_NAME;
+import static com.thinkhr.external.api.services.utils.EmailUtil.FIRST_NAME;
+import static com.thinkhr.external.api.services.utils.EmailUtil.SET_LOGIN_LINK;
+import static com.thinkhr.external.api.services.utils.EmailUtil.SET_PASSWORD_LINK;
+import static com.thinkhr.external.api.services.utils.EmailUtil.SUPPORT_EMAIL;
+import static com.thinkhr.external.api.services.utils.EmailUtil.SUPPORT_PHONE;
+import static com.thinkhr.external.api.services.utils.EmailUtil.USER_NAME;
+import static com.thinkhr.external.api.services.utils.EmailUtil.prepareResetPasswordlink;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.springml.marketo.rest.client.LeadDatabaseClient;
+import com.springml.marketo.rest.client.MarketoClientFactory;
+import com.springml.marketo.rest.client.model.QueryResult;
+import com.thinkhr.external.api.db.entities.Company;
+import com.thinkhr.external.api.db.entities.User;
+import com.thinkhr.external.api.exception.APIErrorCodes;
+import com.thinkhr.external.api.exception.ApplicationException;
 import com.thinkhr.external.api.model.EmailRequest;
+import com.thinkhr.external.api.model.KeyValuePair;
+import com.thinkhr.external.api.model.RequestCampaign;
+import com.thinkhr.external.api.repositories.CompanyRepository;
+import com.thinkhr.external.api.repositories.EmailTemplateRepository;
+import com.thinkhr.external.api.repositories.SetPasswordRequestRepository;
+import com.thinkhr.external.api.repositories.UserRepository;
 
 import lombok.Data;
 
@@ -15,17 +45,116 @@ import lombok.Data;
  */
 @Data
 public class MarketoEmailService implements EmailService {
+    
+    private Logger logger = LoggerFactory.getLogger(MarketoEmailService.class);
+    
+    @Autowired
+    private EmailTemplateRepository emailRepository;
+    
+    @Autowired
+    private SetPasswordRequestRepository setPasswordRepository;
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+    private CompanyRepository companyRepository;
 
+    @Value("${sendgrid_api_key}")
+    private String apiKey;
+
+    @Value("${sendgrid_key_name}")
+    private String keyName;
+
+    @Value("${sendgrid_auth_template_id}")
+    private String authTemplateId;
+    
+    @Value("${sendgrid_channel_template_id}")
+    private String channelTemplateId;
+    
+    @Value("${default_support_email}")
+    private String defaultSupportEmail;
+    
+    @Value("${default_support_phone}")
+    private String defaultSupportPhone;
+    
+    @Value("${login_url}")
+    private String loginUrl;
+    
     @Override
     public EmailRequest createEmailRequest(Integer brokerId, List<String> userNames) {
-        // TODO Auto-generated method stub
-        return null;
+        EmailRequest emailRequest = new EmailRequest();
+        Company broker = null;
+        if (brokerId != null) {
+            broker = companyRepository.findOne(brokerId);
+        }
+        
+        
+        for(String username : userNames ) {
+            User user = userRepository.findByUserName(username);
+            if (user == null) {
+                throw ApplicationException.createEntityNotFoundError(APIErrorCodes.ENTITY_NOT_FOUND, "username", username);
+            }
+
+            //String sendgridTemplateId = emailTemplate.getSendgridTemplateId();
+            String generatedHashedCode = RESET_PASSWORD_PREFIX + generateHashedValue(user.getUserId());
+            String resetPasswordLink = prepareResetPasswordlink(loginUrl, generatedHashedCode);
+
+
+            List<KeyValuePair> substitutions = new ArrayList<KeyValuePair>();
+            substitutions.add(createKeyValue(SET_LOGIN_LINK, loginUrl));
+            substitutions.add(createKeyValue(FIRST_NAME, user.getFirstName()));
+            substitutions.add(createKeyValue(BROKER_NAME, broker.getCompanyName()));
+            substitutions.add(createKeyValue(USER_NAME, user.getUserName()));
+            substitutions.add(createKeyValue(SUPPORT_PHONE, defaultSupportPhone));
+            substitutions.add(createKeyValue(SUPPORT_EMAIL, defaultSupportEmail));
+            substitutions.add(createKeyValue(SET_PASSWORD_LINK, resetPasswordLink));
+            
+            emailRequest.getRecipientToSubstitutionMap().put(user, substitutions);
+        }
+        
+        return emailRequest;
     }
 
     @Override
     public void sendEmail(EmailRequest emailRequest) throws Exception {
-        // TODO Auto-generated method stub
+        String clientId = null;
+        String clientSecret = null;
+        String baseUri = null;
+        Integer campaignId = null;
+     
+        LeadDatabaseClient leadClient = MarketoClientFactory.getLeadDatabaseClient(clientId, clientSecret, baseUri);
+        String accessToken = "token"; //TODO write code to get this
+        
+        for (User userToSendEmail : emailRequest.getRecipientToSubstitutionMap().keySet()) {
+            RequestCampaign requestCampaign = new RequestCampaign(baseUri, campaignId);
+            
+            QueryResult result = leadClient.query("leads", "email", userToSendEmail.getEmail());
+            Integer leadId = Integer.valueOf(result.getResult().get(0).get("id"));
 
+            // Set tokens
+            List<KeyValuePair> subtitutions = emailRequest.getRecipientToSubstitutionMap().get(userToSendEmail);
+            if (subtitutions != null && !subtitutions.isEmpty()) {
+                subtitutions.stream().forEach(keyValuePair -> {
+                    requestCampaign.addToken(keyValuePair.getKey(), keyValuePair.getValue());
+                });
+            }
+            
+            requestCampaign.addLead(leadId);
+            requestCampaign.postData(accessToken);
+        }
+    }
+    
+    /**
+     * Create KeyValue pair
+     * 
+     * @param key
+     * @param value
+     * @return
+     */
+    private KeyValuePair createKeyValue(String key,
+            String value) {
+        return new KeyValuePair(key, value);
     }
 
 }
