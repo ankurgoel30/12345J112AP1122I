d49f9c8f4170b5a99af10adadb7a5416f6943979
jnajayjain <ajay.jain@pepcus.com>
2018-02-01 13:00:15 +0530

    THR-4278: Externalized invalid date message.

--- src/main/java/com/thinkhr/external/api/helpers/JsonDateDeSerializer.java
+++ src/main/java/com/thinkhr/external/api/helpers/JsonDateDeSerializer.java
@@ -1,26 +1,38 @@
 package com.thinkhr.external.api.helpers;
 
+import static com.thinkhr.external.api.response.APIMessageUtil.getMessageFromResourceBundle;
+
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.thinkhr.external.api.exception.APIErrorCodes;
+import com.thinkhr.external.api.exception.MessageResourceHandler;
 
 /**
  * This is custom deserializer class to deserialize date values in json payload
  * @author Ajay Jain
  *
  */
+@Component
 public class JsonDateDeSerializer extends JsonDeserializer<Date> {
+
+    @Autowired
+    protected MessageResourceHandler resourceHandler;
 
     @Override
     public Date deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String datePattern = "yyyy-MM-dd";
+        SimpleDateFormat format = new SimpleDateFormat(datePattern);
         format.setLenient(false);
 
         String date = p.getText();
@@ -28,8 +40,10 @@ public class JsonDateDeSerializer extends JsonDeserializer<Date> {
         try {
             return format.parse(date);
         } catch (ParseException e) {
-            String customMessage = ".Date is not in format 'yyyy-MM-dd' or date is invalid calender date";
-            throw new JsonParseException(e.getLocalizedMessage() + customMessage, null, e);
+            String customMessage = getMessageFromResourceBundle(resourceHandler, APIErrorCodes.DATE_PARSE_ERROR,
+                    datePattern);
+
+            throw new JsonParseException(e.getLocalizedMessage() + ". " + customMessage, null, e);
         }
     }
 }
