fc696926477fe243295a063adf60d0f2c1c24dbf
jnajayjain <ajay.jain@pepcus.com>
2018-02-15 16:46:33 +0530

    THR-4494: Fixed Bug 4494

--- src/main/java/com/thinkhr/external/api/repositories/SkuRepository.java
+++ src/main/java/com/thinkhr/external/api/repositories/SkuRepository.java
@@ -1,6 +1,9 @@
 package com.thinkhr.external.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.thinkhr.external.api.db.entities.Sku;
@@ -13,5 +16,8 @@ import com.thinkhr.external.api.db.entities.Sku;
  *
  */
 public interface SkuRepository extends PagingAndSortingRepository<Sku, Integer>, JpaSpecificationExecutor<Sku> {
+
+    @Query(value = "select s.id from Sku s")
+    public List<Integer> findAllSkus();
 
 }
